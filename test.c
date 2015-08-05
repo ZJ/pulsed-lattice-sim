04cff4ad96e2aae1ad4909c0d1ab4d4c3701c31a
Zach Smith <zsmith12@umd.edu>
2015-08-05 12:02:57 -0400

    Apply indent to test.c

--- test.c
+++ test.c
@@ -5,10 +5,10 @@
 #define N 4096
 
 typedef enum memOpRetCodes = {
-	OK,                 //!< No Error
-	ERR_MEM_ALLOC,      //!< Problem reported by allocation routines
-	ERR_MEM_NOSIZE,     //!< Needed size field missing/zero
-	ERR_MEM_NULLPTR     //!< Required pointer had a NULL value
+    OK,				       //!< No Error
+    ERR_MEM_ALLOC,		       //!< Problem reported by allocation routines
+    ERR_MEM_NOSIZE,		       //!< Needed size field missing/zero
+    ERR_MEM_NULLPTR		       //!< Required pointer had a NULL value
 } memRetCodes_type;
 
 
@@ -27,56 +27,61 @@ typedef enum memOpRetCodes = {
  *	@param[in]		colWaist	Scaled Gaussian waist along a column
  *	@param[in]		amplitude	Amplitude of the Gaussian
  */
-memRetCodes_type initGaussianEnvelope (
-	fftw_complex ** inputArray,
-	const unsigned int numRows,
-	const unsigned int numCols,
-	const double rowWaist,
-	const double colWaist,
-	const double amplitude
+memRetCodes_type initGaussianEnvelope(
+    fftw_complex ** inputArray,
+    const unsigned int numRows,
+    const unsigned int numCols,
+    const double rowWaist,
+    const double colWaist,
+    const double amplitude
 ) {
-	int i = 0;
-	int j = 0;
-	double iCenter;
-	double jCenter;
-	const double rowDenom = 2*rowWaist*rowWaist;
-	const double colDenom = 2*colWaist*colWaist;
-	unsigned int numCells = numRows * numCols;
+    int                           i = 0;
+    int                           j = 0;
+    double                        iCenter;
+    double                        jCenter;
+    const double                  rowDenom = 2 * rowWaist * rowWaist;
+    const double                  colDenom = 2 * colWaist * colWaist;
+    unsigned int                  numCells = numRows * numCols;
 
-	// Check for improperly formatted size info
-	if ( 0 == numRows ) {
-		return ERR_MEM_NOSIZE;
-	}
+    // Check for improperly formatted size info
+    if (0 == numRows) {
+	return ERR_MEM_NOSIZE;
+    }
+    // Handle NULL ptr
+    if (NULL == *inputArray) {
+	fftw_complex                 *tempPtr = NULL;
 
-	// Handle NULL ptr
-	if ( NULL == *inputArray ) {
-		fftw_complex * tempPtr = NULL;
-		tempPtr = fftw_alloc_complex(numCells);
-		if ( NULL == tempPtr ) {
-			return ERR_MEM_ALLOC;
-		}
-		*inputArray = tempPtr;
+	tempPtr = fftw_alloc_complex(numCells);
+	if (NULL == tempPtr) {
+	    return ERR_MEM_ALLOC;
 	}
-	
-	// Do the Gaussian
-	iCenter = (double numCols)/2.0;
-	jCenter = (double numRows)/2.0;
-	for ( i=0; i < numRows; i++ ) {
-		double rowAmp = amplitude * exp(-((i-iCenter)*(i-iCenter))/rowDenom);
-		int iOffset = i*numCols;
-		for ( j=0; j < numCols; j++ ) {
-			*(*inputArray + iOffset + j) = rowAmp * exp(-((j-jCenter)*(j-jCenter))/colDenom);
-		}
+	*inputArray = tempPtr;
+    }
+    // Do the Gaussian
+    iCenter = (double numCols) /2.0;
+    jCenter = (double numRows) /2.0;
+    for (i = 0; i < numRows; i++) {
+	double                        rowAmp =
+	    amplitude * exp(-((i - iCenter) * (i - iCenter)) / rowDenom);
+	int                           iOffset = i * numCols;
+
+	for (j = 0; j < numCols; j++) {
+	    *(*inputArray + iOffset + j) =
+		rowAmp * exp(-((j - jCenter) * (j - jCenter)) / colDenom);
 	}
-	
-	return OK;
+    }
+
+    return OK;
 }
 
-int main(int argc, char ** argv) {
-	fftw_complex * testPtr = NULL;
-	int i = 0;
-	
-	i++;
-	testPtr = fftw_alloc_complex(N*N);
-	printf("Pointer:\t0x%p\n", testPtr);
+int main(
+    int argc,
+    char **argv
+) {
+    fftw_complex                 *testPtr = NULL;
+    int                           i = 0;
+
+    i++;
+    testPtr = fftw_alloc_complex(N * N);
+    printf("Pointer:\t0x%p\n", testPtr);
 }
